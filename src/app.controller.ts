2427ed0dceb0cf6bbda6732a7cb7f075fa3817bf
Hugo Barboza <hugoebarboza@gmail.com>
2021-02-25 16:35:54 -0300

    ODIS-557 BACK - NestJs configurar rutas de prueba

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+// Allow only request from domain.com/odis
+@Controller('odis')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
   }
