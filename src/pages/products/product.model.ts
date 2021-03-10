ea2998fa5e6f3f020fbcffaba8b8d1476bdb5704
Hugo Barboza <hugoebarboza@gmail.com>
2021-03-10 17:49:48 -0300

    ODIS-608 BACK - Configurar Swagger UI Documentation

--- src/pages/products/product.model.ts
+++ src/pages/products/product.model.ts
@@ -1,8 +1,46 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class Product {
+
+    @ApiProperty({
+        type: 'string',
+        name: 'id',
+        description: 'id of Product'
+    })
+    id: string;
+
+    @ApiProperty({
+        type: 'string',
+        name: 'title',
+        description: 'title of Product'
+    })
+    title: string;
+
+    @ApiProperty({
+        type: 'string',
+        name: 'description',
+        description: 'description of Product'
+    })
+    description: string;
+
+    @ApiProperty({
+        type: 'string',
+        name: 'price',
+        description: 'price of Product'
+    })
+    price: number;
+
+
+
     constructor(
-        public id: string,
-        public title: string,
-        public description: string,
-        public price: number,
-    ) { }
+        id: string,
+        title: string,
+        description: string,
+        price: number,
+    ) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+    }
 }
