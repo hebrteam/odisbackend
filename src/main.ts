57865185adcf2edc323d0141d2223e5a02053a3f
Hugo Barboza <hugoebarboza@gmail.com>
2021-03-11 13:25:19 -0300

    DIS-562 BACK - Configuración de microservicios

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,35 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
+// Create a logger instance
+const logger = new Logger('Main');
+
+// Create the microservice options object
+
+const microserviceOptions: MicroserviceOptions = {
+  transport: Transport.TCP,
+  options: {
+    host: '127.0.0.1',
+    port: 4200,
+  },
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ? Number(process.env.PORT) : 8080;
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, microserviceOptions);
+  app.listen(() => { logger.log('Microservice is listening.....') });
 
+  /*
+    const options = new DocumentBuilder()
+      .setVersion('1.0')
+      .build();
+  
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('/docs', app, document);*/
 
-  const options = new DocumentBuilder()
-    .setVersion('1.0')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('/docs', app, document);
-
-  await app.listen(3000);
+  // await app.listen(3000);
+  // await app.listen(() => console.log('Microservice listening on port:', port));
 }
 bootstrap();
